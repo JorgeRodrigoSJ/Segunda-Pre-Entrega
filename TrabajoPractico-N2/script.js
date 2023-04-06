9b46c2d164437eb7e4bc85c10608fc3ed3a1e263
JorgeRodrigoSJ <121474120+JorgeRodrigoSJ@users.noreply.github.com>
2023-04-05 23:23:30 -0300

    Add files via upload

--- TrabajoPractico-N2/script.js
+++ TrabajoPractico-N2/script.js
@@ -19,4 +19,75 @@ Arrays.
 Métodos de búsqueda y filtrado sobre el Array.
 
 
----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+*/
+
+//SELECTOR DE REGALOS
+
+
+const regalos = [
+    { id: 1, nombre: "mate", precio: 1200, img: "mate.jpg" },
+    { id: 2, nombre: "masajes", precio: 1800, img: "masajes.jpg" },
+    { id: 3, nombre: "remera", precio: 2800, img: "remera.jpg" },
+    { id: 4, nombre: "campera", precio: 1500, img: "campera.webp" },
+    { id: 5, nombre: "viaje por el pais", precio: 1100, img: "viaje.webp" },
+    { id: 6, nombre: "mochila", precio: 1900, img: "mochila.jpeg" },
+    { id: 7, nombre: "gorra", precio: 5800, img: "gorra.png" },
+   
+  ];
+
+
+let maximo = prompt("Selecciona cuanto es lo maximo que desea gastar en su regalo:");
+
+console.log("eligio el rango hasta $"+maximo);
+
+//funcion de filtrado
+function filtrarPorPrecio(arr, filtro){
+    return arr.filter((el)=>{
+      return el.precio <= filtro
+    })
+  }
+
+const filtrados = filtrarPorPrecio(regalos, maximo)
+
+console.log(filtrados);
+
+
+  
+
+//FUNCION CONSTRUCTOR
+function regalo(nombre, precio, img) {
+    this.id = regalos.length + 1;
+    this.nombre = nombre;
+    this.precio = parseFloat(precio);
+    //valido si recibo imagen
+    if (!img) {
+      this.img = "https://via.placeholder.com/300";
+    } else {
+      this.img = img;
+    }
+  }
+
+
+//esto es para agregar regalos  
+regalos.push (new regalo("camisa",5000));
+
+
+
+/*aca pueo hacer que sume iva
+
+sumaIva() {
+    this.precio = this.precio*1.21;
+}
+
+//sumar iva a los regalos
+
+for (const regalo of regalos)
+regalos.sumaIva():
+
+console.log(regalos);
+   
+ 
+*/
+
+
